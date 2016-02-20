0668ef082c4c2eba21bc1631bcca98b030973738
Shouko <h999342@gmail.com>
2016-02-20 18:36:10 +0800

    use two colums

--- gen.js
+++ gen.js
@@ -3,12 +3,14 @@ var fs = require('fs');
 var law_text = fs.readFileSync('./laws/' + process.argv[process.argv.length - 1], 'utf-8');
 var law_data = law_text.split("\n\n");
 var latex_final = fs.readFileSync('header.tex', 'utf-8');
-latex_final += '\\newpage \\section{' + law_data[0] + '}'; // 法名
 
 // 修訂紀錄
+latex_final += '\\newpage \\section{' + law_data[0] + '}'; // 法名
 latex_final += "{\\fontsize{9pt}{13pt} \\selectfont\n";
 latex_final += law_data[1].split("\n").join("\\\\\n");
 latex_final += "}\\\\\n\n";
+
+latex_final += "\\begin{multicols}{2}\n";
 
 // 內文
 var formatted_lines = law_data[2].split("\n").map(function(element, index, array) {
@@ -19,15 +21,15 @@ var formatted_lines = law_data[2].split("\n").map(function(element, index, array
 		case 1: // 條
 			return "\\textbf{" + element + "}\\\\";
 		case 2: // 項
-			return "\\indent " + element + "\\\\";
+			return "\\hspace*{15pt} " + element + "\\\\";
 		case 3: // 款
-			return "\\indent \\indent " + element + "\\\\";
+			return "\\hspace*{30pt} " + element + "\\\\";
 		default:
 			return "";
 	}
 });
 
 latex_final += formatted_lines.join("\n");
-latex_final += "\\end{document}";
+latex_final += "\\end{multicols} \\end{document}";
 
 console.log(latex_final);
