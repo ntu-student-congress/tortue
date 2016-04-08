1dbf50697a40fd06ef4dd7e2f9815d8470ae7d60
shouko <imshouko@gmail.com>
2016-04-08 17:11:04 +0800

    seperate gen.js to gen.js and gen_full.js

--- gen.js
+++ gen.js
@@ -1,57 +1,37 @@
 var fs = require('fs');
 
-fs.readdir("laws", function(err, law_cats) {
-	var cats_latex = law_cats.map(function(law_cat) {
-		var stat = fs.statSync("laws/" + law_cat);
-		if(!stat || !stat.isDirectory()) return;
-		var cat_latex = '\\cleardoublepage \\vspace*{\\fill}\\begin{center} \\fontsize{80pt}{100pt} \\fontnoto ' + law_cat.split('_')[1] + '\\end{center}\\vspace*{\\fill}\n';
-		var laws_fn = fs.readdirSync("laws/" + law_cat);
-		var laws = [];
-		laws_fn.forEach(function(law_in_cat) {
-			laws.push(law_cat + "/" + law_in_cat);
-		});
-		var laws_latex = laws.map(function(law) {
-			var law_data = fs.readFileSync("laws/" + law, 'utf-8').replace(/\r\n/g,"\n").split("\n\n");
-			var law_latex = "\\cleardoublepage \\section*{\\fontml " + law_data[0] + "}\n\\addcontentsline{toc}{section}{" + law_data[0] + "}"; // 法名
+module.exports = function(law) {
+  var law_data = fs.readFileSync("laws/" + law, 'utf-8').replace(/\r\n/g,"\n").split("\n\n");
+  var law_latex = "\\cleardoublepage \\section*{\\fontml " + law_data[0] + "}\n\\addcontentsline{toc}{section}{" + law_data[0] + "}"; // 法名
 
-			// 修訂紀錄
-			law_latex += "{\\fontsize{9pt}{13pt} \\fontml \\selectfont\n";
-			law_latex += law_data[1].replace(/　+/g, '').split("\n").join("\\\\\n");
-			law_latex += "}\n\n";
+  // 修訂紀錄
+  law_latex += "{\\fontsize{9pt}{13pt} \\fontml \\selectfont\n";
+  law_latex += law_data[1].replace(/　+/g, '').split("\n").join("\\\\\n");
+  law_latex += "}\n\n";
 
-			law_latex += "\\begin{multicols*}{2}\n";
+  law_latex += "\\begin{multicols*}{2}\n";
 
-			// 內文
-			var formatted_lines = law_data[2].split("\n").map(function(element, index, array) {
-				if(element.length > 1 && element[0] != " ") { // 章
-					return "\\subsection*{" + element + "}";
-				}
-				var text = element.replace(/^\s+/, '');
-				switch(element.search(/\S/) / 2){
-					case 1: // 條
-						return "\\vspace{0.15em}\\textbf{" + text.replace('【', '【{\\fontml ').replace('】', '}】') + "}\\\\";
-					case 2: // 項
-						return "\\begingroup\\raggedright\\leftskip=0em\\vspace{0em}　　" + text + " \\par\\endgroup\\par\\vspace{0em}";
-					case 3: // 款
-						return "\\begingroup\\raggedright\\leftskip=4em\\hspace{-2em}\\vspace{0em}" + text + " \\par\\endgroup\\par\\vspace{0em}";
-					case 4: // 目
-						return "\\begingroup\\raggedright\\leftskip=6em\\hspace{-2em}\\vspace{0em}" + text + " \\par\\endgroup\\par\\vspace{0em}";
-					default:
-						return "";
-				}
-			});
+  // 內文
+  var formatted_lines = law_data[2].split("\n").map(function(element, index, array) {
+    if(element.length > 1 && element[0] != " ") { // 章
+      return "\\subsection*{" + element + "}";
+    }
+    var text = element.replace(/^\s+/, '');
+    switch(element.search(/\S/) / 2){
+      case 1: // 條
+        return "\\vspace{0.15em}\  extbf{" + text.replace('【', '【{\\fontml ').replace('】', '}】') + "}\\\\";
+      case 2: // 項
+        return "\\begingroup\\raggedright\\leftskip=0em\\vspace{0em}　　" + text + " \\par\\endgroup\\par\\vspace{0em}";
+      case 3: // 款
+        return "\\begingroup\\raggedright\\leftskip=4em\\hspace{-2em}\\vspace{0em}" + text + " \\par\\endgroup\\par\\vspace{0em}";
+      case 4: // 目
+        return "\\begingroup\\raggedright\\leftskip=6em\\hspace{-2em}\\vspace{0em}" + text + " \\par\\endgroup\\par\\vspace{0em}";
+      default:
+        return "";
+    }
+  });
 
-			law_latex += formatted_lines.join("\n");
-			law_latex += "\\end{multicols*}";
-			return law_latex;
-		});
-		cat_latex += laws_latex.join("\n");
-		return cat_latex;
-	});
-
-	var latex_final = fs.readFileSync('front.tex', 'utf-8');
-	latex_final += fs.readFileSync('cover.tex', 'utf-8');
-	latex_final += cats_latex.join("\n\n");
-	latex_final += fs.readFileSync('back.tex', 'utf-8');
-	console.log(latex_final);
-});
+  law_latex += formatted_lines.join("\n");
+  law_latex += "\\end{multicols*}";
+  return law_latex;
+};
